29b4f05dca96703205697127a0705041ea60ace7
MarioCaly <alejandro239999986@gmail.com>
2023-08-03 10:43:49 -0300

    modificacion app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let express=require("express")
+let app=express();
+let path=require("path")
+app.listen("3000", function () {
+    console.log("Servidor corriendo")
+    
+})
+app.use(express.static("public"))
+app.use(express.static("views"))
+app.get("/",function (req,res) {
+    res.sendFile(path.join(__dirname,"index.html"))
+    
+})
