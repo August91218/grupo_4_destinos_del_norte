3fcc024be0e4e24dbb9cc55b0b6b3db3596d002b
MarioCaly <alejandro239999986@gmail.com>
2023-08-04 10:11:43 -0300

    modificaciones en el index y css

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ let app=express();
 let path=require("path");
 app.listen("3000", function () {
     console.log("Servidor corriendo")
-    
-})
+});
 app.use(express.static("public"))
-app.use(express.static("views"))
 app.get("/",function (req,res) {
-    res.sendFile(path.join(__dirname,"./index.html"))
-    
+
+    res.sendFile(path.join(__dirname,"index.html"))
 })
