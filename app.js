333a25cc36c32e8f27623f0afa00381784da724a
JoaquinZerpa <137903803+JoaquinZerpa@users.noreply.github.com>
2023-08-07 20:22:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.get("/",function (req,res) {
 
     res.sendFile(path.join(__dirname,"index.html"))
 })
+//Para la funcion de tienda y carrito donde los productos comprados se manden al carrito donde podran ser vistos//
+const header = document.querySelector("header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll",function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
